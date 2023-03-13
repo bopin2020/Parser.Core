ddc862938a680c07ad8159b7e7218f69abf55589
bopin2020 <2963977354@qq.com>
2023-03-13 23:06:54 +0800

    add EventMapTable

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/EventMapTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// II.22.12 EventMap : 0x12 
+    /// page 246
+    /// 
+    /// Note that EventMap info does not directly influence runtime behavior; what counts is the information
+    /// stored for each method that the event comprises.
+    /// .event directive
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.EventMap)]
+    [MetadataTableLevel(MetadataTableLevel.Important)]
+    public struct EventMapTable
+    {
+        /// <summary>
+        /// an index into the TypeDef table
+        /// </summary>
+        public int Parent { get; set; }
+
+        /// <summary>
+        /// an index into the Event table
+        /// </summary>
+        public int EventList { get; set; }
+    }
+}
