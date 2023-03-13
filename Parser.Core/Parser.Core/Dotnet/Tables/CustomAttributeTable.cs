430882e58e952eccca9f7e161db05adf298af561
bopin2020 <2963977354@qq.com>
2023-03-13 22:55:36 +0800

    add CustomAttributeTable

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/CustomAttributeTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// II.22.10 CustomAttribute : 0x0C 
+    /// page 242
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.CustomAttribute)]
+    [MetadataTableLevel(MetadataTableLevel.Important)]
+    public struct CustomAttributeTable
+    {
+        /// <summary>
+        /// an index into a metadata table that has an associated HasCustomAttribute
+        /// (§II.24.2.6) coded index
+        /// 
+        /// .custom
+        /// </summary>
+        public int Parent { get; set; }
+
+        /// <summary>
+        /// an index into the MethodDef or MemberRef table
+        /// 
+        /// The column called Type is slightly misleading
+        /// it actually indexes a constructor method
+        /// </summary>
+        public int Type { get; set; }
+        /// <summary>
+        /// an index into the Blob heap
+        /// </summary>
+        public int Value { get; set; }
+    }
+}
