1ac313dbfbedb6b3a9a65b62e4746acd89164221
bopin2020 <2963977354@qq.com>
2023-03-14 14:16:00 +0800

    add TypeSpec

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/TypeSpec.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// page 274
+    /// 
+    /// The TypeSpec table has just one column, which indexes the specification of a Type, stored in the Blob
+    ///heap
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.TypeSpec)]
+    [MetadataTableLevel(MetadataTableLevel.Sophisticated)]
+    public struct TypeSpec
+    {
+        /// <summary>
+        /// index into the Blob heap, where the blob is formatted as specified
+        /// </summary>
+        public int Signature { get; set; }
+    }
+}
