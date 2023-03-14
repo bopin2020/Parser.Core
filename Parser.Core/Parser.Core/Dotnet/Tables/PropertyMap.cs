a0b41a32428dd86e5a3f3673c3cfcc03a213c5ed
bopin2020 <2963977354@qq.com>
2023-03-14 13:39:31 +0800

    add PropertyMap

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/PropertyMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// page 268
+    /// 
+    /// .property
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.PropertyMap)]
+    [MetadataTableLevel(MetadataTableLevel.Sophisticated)]
+    public struct PropertyMap
+    {
+        /// <summary>
+        /// an index into the TypeDef table
+        /// </summary>
+        public int Parent { get; set; }
+        /// <summary>
+        /// an index into the Property table
+        /// </summary>
+        public int PropertyList { get; set; }
+    }
+}
