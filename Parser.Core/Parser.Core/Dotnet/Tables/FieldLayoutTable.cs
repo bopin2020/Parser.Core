33e10a4df12842b5f850e8a15d5f975d53dd0647
bopin2020 <2963977354@qq.com>
2023-03-14 11:29:28 +0800

    add FieldLayoutTable

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/FieldLayoutTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// A row in the FieldLayout table is created if the .field directive for the parent field has specified a field
+    ///offset
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.FieldLayout)]
+    [MetadataTableLevel(MetadataTableLevel.Important)]
+    public struct FieldLayoutTable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Offset { get; set; }
+        /// <summary>
+        /// an index into the Field table
+        /// </summary>
+        public int Field { get; set; }
+        /// <summary>
+        /// an index into the Blob heap
+        /// </summary>
+        public int Signature { get; set; }
+    }
+}
