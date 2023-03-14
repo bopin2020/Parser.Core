25feab45e5f011c19e6681d1fb93138f191f10f8
bopin2020 <2963977354@qq.com>
2023-03-14 13:02:51 +0800

    add MethodImplTable

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/MethodImplTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// MethodImpl tables let a compiler override the default inheritance rules provided by the CLI
+    /// 
+    /// .override
+    /// 
+    /// 类的析构函数      实现了Finialize方法 System.Object
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.MethodImpl)]
+    [MetadataTableLevel(MetadataTableLevel.Sophisticated)]
+    public struct MethodImplTable
+    {
+        /// <summary>
+        /// an index into the TypeDef table
+        /// </summary>
+        public int Class { get; set; }
+        /// <summary>
+        /// an index into the MethodDef or MemberRef table; more precisely, a MethodDefOrRef
+        /// </summary>
+        public int MethodBody { get; set; }
+        /// <summary>
+        /// an index into the MethodDef or MemberRef table
+        /// </summary>
+        public int MethodDeclaration { get; set; }
+    }
+}
