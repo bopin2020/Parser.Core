f58b0cd5d90512cf5d64d5649c0ad0affabd404c
bopin2020 <2963977354@qq.com>
2023-03-14 13:27:02 +0800

    add ModuleRef

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/ModuleRef.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// .module extern
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.ModuleRef)]
+    [MetadataTableLevel(MetadataTableLevel.Sophisticated)]
+    public struct ModuleRef
+    {
+        /// <summary>
+        /// an index into the String heap
+        /// 
+        /// Name should match an entry in the Name column of the File table
+        /// </summary>
+        public int Name { get; set; }
+    }
+}
