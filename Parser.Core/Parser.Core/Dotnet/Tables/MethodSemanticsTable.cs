abbe5096103d909e232bd1ec5d0b66abba9592b0
bopin2020 <2963977354@qq.com>
2023-03-14 13:13:46 +0800

    add MethodSemanticsTable

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/MethodSemanticsTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// .property
+    /// .event
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.MethodSemantics)]
+    [MetadataTableLevel(MetadataTableLevel.Sophisticated)]
+    public struct MethodSemanticsTable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MethodSemanticsAttributes Semantics { get; set; }
+        /// <summary>
+        /// an index into the MethodDef table
+        /// </summary>
+        public int Method { get; set; }
+        /// <summary>
+        /// an index into the Event or Property table
+        /// </summary>
+        public int Association { get; set; }
+    }
+}
