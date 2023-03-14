a0fa26919ede3d1ff020f0db164070d8bf16084f
bopin2020 <2963977354@qq.com>
2023-03-14 14:11:00 +0800

    add TypeRefTable

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/TypeRefTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.TypeRef)]
+    [MetadataTableLevel(MetadataTableLevel.Sophisticated)]
+    public struct TypeRefTable
+    {
+        /// <summary>
+        /// an index into a Module, ModuleRef, AssemblyRef or TypeRef table,
+        /// or null; more precisely, a ResolutionScope
+        /// </summary>
+        public int ResolutionScope { get; set; }
+        /// <summary>
+        /// an index into the String heap
+        /// </summary>
+        public int TypeName { get; set; }
+        /// <summary>
+        /// an index into the String heap
+        /// 
+        /// TypeNamespace can be null, or non-null
+        /// </summary>
+        public int TypeNamespace { get; set; }
+    }
+}
