4302a09d9314dca9daddbd30efde91ec94db3507
bopin2020 <2963977354@qq.com>
2023-03-14 13:23:11 +0800

    update ModuleTable

--- Parser.Core/Parser.Core/Dotnet/Tables/ModuleTable.cs
+++ Parser.Core/Parser.Core/Dotnet/Tables/ModuleTable.cs
@@ -10,7 +10,29 @@ namespace Parser.Core.Dotnet.Tables
     /// 
     /// </summary>
     [MetadataTableTypeDef(MetadataTableType.Module)]
+    [MetadataTableLevel(MetadataTableLevel.Sophisticated)]
     public struct ModuleTable
     {
+        /// <summary>
+        /// a 2-byte value, reserved, shall be zero
+        /// </summary>
+        public short Generation { get; set; }
+        /// <summary>
+        /// an index into the String heap
+        /// </summary>
+        public int Name { get; set; }
+        /// <summary>
+        /// an index into the Guid heap; simply a Guid used to distinguish between two
+        /// versions of the same module)
+        /// </summary>
+        public int Mvid { get; set; }
+        /// <summary>
+        /// an index into the Guid heap; reserved, shall be zero
+        /// </summary>
+        public int EncId { get; set; }
+        /// <summary>
+        /// an index into the Guid heap; reserved, shall be zero
+        /// </summary>
+        public int EncBaseId { get; set; }
     }
 }
