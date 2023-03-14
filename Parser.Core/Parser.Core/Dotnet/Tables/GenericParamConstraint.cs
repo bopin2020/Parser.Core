b13510f102bc801158456c61a864a13c4c790361
bopin2020 <2963977354@qq.com>
2023-03-14 11:56:04 +0800

    add GenericParamConstraint

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/GenericParamConstraint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// II.22.21 GenericParamConstraint : 0x2C
+    /// page 255
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.GenericParamConstraint)]
+    [MetadataTableLevel(MetadataTableLevel.Important)]
+    public struct GenericParamConstraint
+    {
+        /// <summary>
+        /// an index into the GenericParam table, specifying to which generic
+        ///parameter this row refers
+        /// </summary>
+        public int Owner { get; set; }
+        /// <summary>
+        /// an index into the TypeDef, TypeRef, or TypeSpec tables, specifying from
+        /// which class this generic parameter is constrained to derive
+        /// 
+        /// or which interface this
+        /// generic parameter is constrained to implement
+        /// </summary>
+        public int Constraint { get; set; }
+    }
+}
