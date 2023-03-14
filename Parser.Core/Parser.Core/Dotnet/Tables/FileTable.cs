547659977cc7ed0d1add1b802b491a1b2d55c7d6
bopin2020 <2963977354@qq.com>
2023-03-14 11:46:37 +0800

    add FileTable

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/FileTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// II.22.19 File : 0x26 
+    /// page 253
+    /// 
+    /// .file
+    /// 
+    /// If the File table is empty, then this, by definition, is a single-file assembly. In this
+    /// case, the ExportedType table should be empty
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.File)]
+    [MetadataTableLevel(MetadataTableLevel.Important)]
+    public struct FileTable
+    {
+        /// <summary>
+        /// </summary>
+        public FileAttributes Flags { get; set; }
+        /// <summary>
+        /// an index into the String heap
+        /// </summary>
+        public int Name { get; set; }
+        /// <summary>
+        /// an index into the Blob heap
+        /// </summary>
+        public int HashValue { get; set; }
+    }
+}
