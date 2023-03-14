0ae910dc4983a36a070d588866380479cf68a269
bopin2020 <2963977354@qq.com>
2023-03-14 13:17:55 +0800

    add MethodSpecTable

---
+++ Parser.Core/Parser.Core/Dotnet/Tables/MethodSpecTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Core.Dotnet.Tables
+{
+    /// <summary>
+    /// II.22.29 MethodSpec : 0x2B
+    /// The MethodSpec table records the signature of an instantiated generic method
+    /// </summary>
+    [MetadataTableTypeDef(MetadataTableType.MethodSpec)]
+    [MetadataTableLevel(MetadataTableLevel.Sophisticated)]
+    public struct MethodSpecTable
+    {
+        /// <summary>
+        /// an index into the MethodDef or MemberRef table, specifying to which
+        /// generic method this row refers; that is, which generic method this row is an
+        /// instantiation of
+        /// </summary>
+        public int Method { get; set; }
+        /// <summary>
+        /// an index into the Blob heap holding the signature of this instantiation
+        /// </summary>
+        public int Instantiation { get; set; }
+        /// <summary>
+        /// an index into the Event or Property table
+        /// </summary>
+        public int Association { get; set; }
+    }
+}
